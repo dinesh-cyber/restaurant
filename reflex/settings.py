f511cebab5573b407a3fbda304e4635214fd4fef
dinesh <dineshmandava0@gmail.com>
2021-10-31 12:18:26 +0530

    mongog-db

--- reflex/settings.py
+++ reflex/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'reflex.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "resturant_db",
+        }
     }
-}
 
 
 # Password validation
